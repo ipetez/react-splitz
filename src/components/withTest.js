ad90d9323ec9bdfd050497210f0187babb16e919
Ike Peters <ikedpeters21@gmail.com>
2018-11-03 22:31:27 -0700

    disable updateExperiments prop by default

--- src/components/withTest.js
+++ src/components/withTest.js
@@ -5,7 +5,11 @@ import { ExperimentContext } from '../index';
 // Higher-order component used to subscribe a component to the experiments
 // it cares about and maps those experiments to the props of that component.
 // Uses React's context API to achieve this
-export default function withTest(WrappedComponent, mapExperimentsToProps) {
+export default function withTest(
+  WrappedComponent,
+  mapExperimentsToProps,
+  options = {}
+) {
   class TestWrapper extends Component {
     render() {
       const { forwardRef, ...rest } = this.props;
@@ -19,6 +23,12 @@ export default function withTest(WrappedComponent, mapExperimentsToProps) {
               mapExperimentsToProps
             );
 
+            const additionalProps = {};
+
+            if (options.updateExperiments) {
+              additionalProps.updateExperiments = updateExperiments;
+            }
+
             // In addition to adding experiments as props to wrapped component, also pass down any props
             // passed in through to the HOC
             return (
@@ -26,7 +36,7 @@ export default function withTest(WrappedComponent, mapExperimentsToProps) {
                 {...subscribedExperiments}
                 {...rest}
                 ref={forwardRef}
-                updateExperiments={updateExperiments}
+                {...additionalProps}
               />
             );
           }}
