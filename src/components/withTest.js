ffd71b5592fe3d7501d8fecc444319e2eff1bd51
Ike Peters <ikedpeters21@gmail.com>
2018-11-04 20:46:40 -0800

    Use currying for withTest HOC for easier composability

--- src/components/withTest.js
+++ src/components/withTest.js
@@ -5,47 +5,47 @@ import { ExperimentContext } from '../index';
 // Higher-order component used to subscribe a component to the experiments
 // it cares about and maps those experiments to the props of that component.
 // Uses React's context API to achieve this
-export default function withTest(
-  WrappedComponent,
-  mapExperimentsToProps,
-  options = {}
-) {
-  class TestWrapper extends Component {
-    render() {
-      const { forwardRef, ...rest } = this.props;
+export default function withTest(mapExperimentsToProps, options) {
+  options = options || {};
 
-      return (
-        <ExperimentContext.Consumer>
-          {expState => {
-            const { exps, updateExperiments } = expState;
-            const subscribedExperiments = addExperimentsToProps(
-              exps,
-              mapExperimentsToProps
-            );
+  return WrappedComponent => {
+    class TestWrapper extends Component {
+      render() {
+        const { forwardRef, ...rest } = this.props;
 
-            const additionalProps = {};
+        return (
+          <ExperimentContext.Consumer>
+            {expState => {
+              const { exps, updateExperiments } = expState;
+              const subscribedExperiments = addExperimentsToProps(
+                exps,
+                mapExperimentsToProps
+              );
 
-            if (options.updateExperiments) {
-              additionalProps.updateExperiments = updateExperiments;
-            }
+              const additionalProps = {};
 
-            // In addition to adding experiments as props to wrapped component, also pass down any props
-            // passed in through to the HOC
-            return (
-              <WrappedComponent
-                {...subscribedExperiments}
-                {...rest}
-                ref={forwardRef}
-                {...additionalProps}
-              />
-            );
-          }}
-        </ExperimentContext.Consumer>
-      );
+              if (options.updateExperiments) {
+                additionalProps.updateExperiments = updateExperiments;
+              }
+
+              // In addition to adding experiments as props to wrapped component, also pass down any props
+              // passed in through to the HOC
+              return (
+                <WrappedComponent
+                  {...subscribedExperiments}
+                  {...rest}
+                  ref={forwardRef}
+                  {...additionalProps}
+                />
+              );
+            }}
+          </ExperimentContext.Consumer>
+        );
+      }
     }
-  }
 
-  return React.forwardRef((props, ref) => {
-    return <TestWrapper {...props} forwardRef={ref} />;
-  });
+    return React.forwardRef((props, ref) => {
+      return <TestWrapper {...props} forwardRef={ref} />;
+    });
+  };
 }
